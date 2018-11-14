d28a789209f51cc79375884bd442bd6e93876c7c
Renato Isidio <renatosis@corp.globo.com>
2018-11-14 10:53:06 -0200

    Adding license

--- prometheus.sh
+++ prometheus.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Copyright 2018 tsuru authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
 
 if [[ "$TARGET_ENDPOINT" != "" ]]; then sed -i s/localhost:9090/$TARGET_ENDPOINT/g /etc/prometheus/prometheus.yml; fi
 /bin/prometheus $1 $2 $3 $4
